0c6cc88e35282e67aec1b405958f77834692690e
karkonta <130680242+karkonta@users.noreply.github.com>
2024-08-13 22:47:58 +0300

    chore():

--- handlers.py
+++ handlers.py
@@ -17,32 +17,25 @@ async def message_handler(msg: Message):
     name = msg.from_user.username
     fullname = msg.from_user.full_name
     id_tg = msg.from_user.id
-    print(id_tg)
-    print(msg.text)
-    print(msg.entities)
-    print(msg.entities)
+
     if msg.entities:
         for entity in msg.entities:
             if entity.type == 'mention':
+                # Извлекаем username из текста сообщения
                 username = msg.text[entity.offset:entity.offset + entity.length].strip('@')
+                print(username)
                 try:
+                    # Получаем информацию о пользователе по username
                     user = await msg.bot.get_chat(username)
-                    id_tg_mentioned = user.id
+                    id_tg_mentioned = user.id  # ID упомянутого пользователя
                     print(f"ID упомянутого пользователя: {id_tg_mentioned}")
                 except Exception as e:
                     print(f"Ошибка при получении пользователя: {e}")
                     await msg.reply(
                         "Не удалось найти упомянутого пользователя. Пожалуйста, проверьте правильность username.")
                     return  # Завершаем выполнение функции, если произошла ошибка
-    # check = msg.chat.id
-    # print(check)
-    # # name = msg.from_user.mention_markdown(msg.from_user.full_name)
-    # print(name)
-    # print(f'{msg.from_user.id}, {msg.from_user.last_name}, {msg.from_user.first_name}')
-    # print(type(name))
-    # print(msg.from_user.mention_markdown(msg.from_user.full_name))
-    # if name is None:
-    #     name = msg.from_user.mention_markdown(msg.from_user.full_name)
+
+
     await msg.reply(f"{msg.from_user.mention_markdown()}"
                     f"{bot_code.check_new_member(id_tg, name, fullname, group_id=msg.chat.id)}", parse_mode="Markdown")
 
